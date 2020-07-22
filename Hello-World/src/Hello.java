d56891254e23c6880194d683979ed8c8b2e684d4
Nikolay Serafimov <nikolay.n.serafimov@gmail.com>
2020-07-22 13:25:21 +0300

    Adds files form section 3 from java developer course

---
+++ Hello-World/src/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    public static void main (String[] args){
+        System.out.println("Hello World");
+        int nomerche = (4 + 4) * 4 ;
+        int nomerdve = 12, nomertri = nomerche*2;
+        int obsht = nomerche+nomerdve+nomertri;
+        int minushilqda = 1000-obsht;
+        System.out.println(obsht);
+        System.out.println(minushilqda);
+    }
+}
