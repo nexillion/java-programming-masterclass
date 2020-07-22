df57227bde12b609078cae7ebb25f5d01faaa272
Nikolay Serafimov <nikolay.n.serafimov@gmail.com>
2020-07-22 14:23:25 +0300

    Adds code shown in video 25 form java developer course

--- Byte-Short-Int-Long/src/com/company/Main.java
+++ Byte-Short-Int-Long/src/com/company/Main.java
@@ -1,8 +1,29 @@
 package com.company;
-
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int value = 10000;
+        int MininumValueOfInt = Integer.MIN_VALUE;
+        int MaximumValuesOfInt = Integer.MAX_VALUE;
+        System.out.println("Minumum Value of an Integer = " + MininumValueOfInt);
+        System.out.println("Maximum Value of an Integer = " + MaximumValuesOfInt);
+        System.out.println(value);
+        System.out.println("Overflow = " + (MaximumValuesOfInt+1));
+        System.out.println("Underflow = " + (MininumValueOfInt-1));
+
+        byte MaximumValueOfByte = Byte.MAX_VALUE;
+        byte MinumumValueOfByte = Byte.MIN_VALUE;
+        System.out.println("Minimum Value of a Byte = "+ MinumumValueOfByte);
+        System.out.println("Maximum Value of a Byte = "+ MaximumValueOfByte);
+
+        short MaximumValueOfShort = Short.MAX_VALUE;
+        short MinumumValueOfShort = Short.MIN_VALUE;
+        System.out.println("Minimum Value of a Short = "+ MinumumValueOfShort);
+        System.out.println("Maximum Value of a Short = "+ MaximumValueOfShort);
+
+        long MaximumValueOfLong = Long.MAX_VALUE;
+        long MinumumValueOfLong = Long.MIN_VALUE;
+        System.out.println("Minimum Value of a Long = "+ MinumumValueOfLong);
+        System.out.println("Maximum Value of a Long = "+ MaximumValueOfLong);
     }
 }
