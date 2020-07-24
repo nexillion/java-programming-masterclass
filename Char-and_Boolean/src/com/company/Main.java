cf36309d424e84361a83fe79a1736cd44a22c8ac
Nikolay Serafimov <nikolay.n.serafimov@gmail.com>
2020-07-24 11:44:21 +0300

    Adds code form video 30 from java developer course

---
+++ Char-and_Boolean/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        char CharVariable = 'F';
+        char CharVariableInUnicode = '\u0046';
+        System.out.println(CharVariable);
+        System.out.println(CharVariableInUnicode);
+        char CharVariableInUnicodeYen = '\u00A5';
+        System.out.println(CharVariableInUnicodeYen);
+
+        boolean BooleanValueTrue = true;
+        boolean BooleanValueFalse = false;
+        //boolean variable-ite sa true/false var.
+    }
+}
