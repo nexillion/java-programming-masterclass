4ec7157be74df85176776073457feb641a8c00c3
Nikolay Serafimov <nikolay.n.serafimov@gmail.com>
2020-07-24 11:15:22 +0300

    Adds new project for the challenge from video 29 from java dev course

---
+++ Pounds-to-Kilograms-29/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        // 1 pound = 0.45359237 kilograms
+        double PoundsOfStuff = 15.455;
+        double KilogramsOfStuff = PoundsOfStuff*0.45359237;
+        System.out.println("15.455 pounds in kilograms = "+KilogramsOfStuff);
+
+    }
+}
