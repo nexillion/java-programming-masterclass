4cf9e7aaf30d83807f894fe52202bf3ad85114a9
Nikolay Serafimov <nikolay.n.serafimov@gmail.com>
2020-08-29 21:29:02 +0300

    Edit code for coding exercise 29 to more closely resemble required output

--- Coding-Exercise-Sum-Calculator-Fields/src/com/company/Main.java
+++ Coding-Exercise-Sum-Calculator-Fields/src/com/company/Main.java
@@ -4,21 +4,15 @@ public class Main {
 
     public static void main(String[] args)
     {
-        Methods.getFirstNumberWithoutParameters();
-        Methods.getSecondNumberWithoutParameters();
-        Methods.setFirstNumberWithOneParameter();
-        Methods.setSecondNumberWithOneParameter();
+        Calculator calculator = new Calculator();
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4);
+        System.out.println("add = " + calculator.getAdditionResult());
+        System.out.println("subtract = " + calculator.getSubtractionResult());
 
-        double outputDouble = Methods.getAdditionResult();
-        System.out.println("Addition result = " + outputDouble);
-
-        outputDouble = Methods.getSubtractionResult();
-        System.out.println("Subtraction result = " + outputDouble);
-
-        outputDouble = Methods.getMultiplicationResult();
-        System.out.println("Multiplication result = " + outputDouble);
-
-        outputDouble = Methods.getDivisionResult();
-        System.out.println("Division result = " + outputDouble);
+        calculator.setFirstNumber(5.25);
+        calculator.setSecondNumber(0);
+        System.out.println("multiply = " + calculator.getMultiplicationResult());
+        System.out.println("divide = " + calculator.getDivisionResult());
     }
 }
