5bc56ebec3c317e555da5ee2d9a48ac0a2b24951
Nikolay Serafimov <nikolay.n.serafimov@gmail.com>
2020-09-13 22:28:43 +0300

    Add project files for coding exercise 33 from java dev course

---
+++ Coding-Exercise-Carpet-Cost-Calculator/src/com/company/Calculator.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Calculator
+{
+    private Floor floor;
+    private Carpet carpet;
+
+    Calculator(Floor floor, Carpet carpet)
+    {
+        this.floor = floor;
+        this.carpet = carpet;
+    }
+
+    public double getTotalCost()
+    {
+        return (this.carpet.getCost() * this.floor.getArea());
+    }
+}
