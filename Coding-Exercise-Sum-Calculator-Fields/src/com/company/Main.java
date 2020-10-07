6a36455dd6590dec138837b2f1d9e3a068bbc08a
Nikolay Serafimov <nikolay.n.serafimov@gmail.com>
2020-10-07 10:52:56 +0300

    Add project files for coding exercise 30 from java dev course (#3)
    * Edit code for coding exercise 29 to more closely resemble required output
    
    * Add project files for coding exercise 30 from java dev course
    
    * Edit code for exercise 30 - final

--- Coding-Exercise-Sum-Calculator-Fields/src/com/company/Main.java
+++ Coding-Exercise-Sum-Calculator-Fields/src/com/company/Main.java
@@ -4,19 +4,6 @@ public class Main {
 
     public static void main(String[] args)
     {
-<<<<<<< Updated upstream
-        Calculator calculator = new Calculator();
-=======
-        SimpleCalculator calculator = new SimpleCalculator();
->>>>>>> Stashed changes
-        calculator.setFirstNumber(5.0);
-        calculator.setSecondNumber(4);
-        System.out.println("add = " + calculator.getAdditionResult());
-        System.out.println("subtract = " + calculator.getSubtractionResult());
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
         calculator.setFirstNumber(5.25);
         calculator.setSecondNumber(0);
         System.out.println("multiply = " + calculator.getMultiplicationResult());
