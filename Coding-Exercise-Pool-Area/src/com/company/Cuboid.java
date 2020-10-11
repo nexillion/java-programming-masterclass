4b2d57ed0ef91ee7029888d7abffde6c47b5c906
nexillion <nikolay.n.serafimov@gmail.com>
2020-10-11 23:39:57 +0300

    Add project files for coding exercise 36 from java dev course

---
+++ Coding-Exercise-Pool-Area/src/com/company/Cuboid.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Cuboid extends Rectangle
+{
+    private double height;
+
+    public Cuboid(double width, double lenght, double height)
+    {
+        super(width, lenght);
+
+        if(height < 0)
+        {
+            this.height = 0;
+        }
+        else
+        {
+            this.height = height;
+        }
+    }
+
+    public double getHeight()
+    {
+        return height;
+    }
+
+    public double getVolume()
+    {
+        return getArea() * this.height;
+    }
+}
