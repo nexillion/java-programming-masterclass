928783b34405740c072fae31f0061a23223e8619
nexillion <nikolay.n.serafimov@gmail.com>
2021-04-26 11:52:16 +0300

    Modify code to include statement executes from lecture 339

--- DataBases-Test/src/com/company/Main.java
+++ DataBases-Test/src/com/company/Main.java
@@ -2,10 +2,7 @@ package com.company;
 
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
 
@@ -17,8 +14,32 @@ public class Main {
             Connection connection = DriverManager.getConnection("jdbc:sqlite:" + path + "\\testDB.db");
 
             Statement statement = connection.createStatement();
-            statement.execute("CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT)");
+            statement.execute("CREATE TABLE IF NOT EXISTS contacts " +
+                    "(name TEXT, phone INTEGER, email TEXT)");
 
+//            statement.execute("INSERT INTO contacts (name, phone, email)" +
+//                    "VALUES ('Nikolay', 79846513, email@email.org)");
+//            statement.execute("INSERT INTO contacts (name, phone, email)" +
+//                    "VALUES ('Joe', 11111, 'joe@email.org')");
+//            statement.execute("INSERT INTO contacts (name, phone, email)" +
+//                    "VALUES ('Frederico', 12345678, 'fred@email.org')");
+//            statement.execute("INSERT INTO contacts (name, phone, email)" +
+//                    "VALUES ('CarrotMan', 852, 'carrot@email.org')");
+
+//            statement.execute("UPDATE contacts SET phone = 22222 WHERE name = 'Joe'");
+
+//            statement.execute("DELETE FROM contacts WHERE name = 'Frederico'");
+
+            statement.execute("SELECT * FROM contacts");
+            ResultSet result = statement.getResultSet();
+            while(result.next())
+            {
+                System.out.println(result.getString("name") + " || " +
+                        result.getInt("phone") + " || " +
+                        result.getString("email"));
+            }
+
+            result.close();
             statement.close();
             connection.close();
         }
